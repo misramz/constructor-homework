d8bf4f3a5ea1870422a8daa786c886199f3435c4
Nisa Sanyang <misramz@yahoo.com>
2016-05-10 08:20:18 -0400

    final commit

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -4,5 +4,7 @@ let Dog = function (x) {
   this.status = x.status || 'normal';
   this.color = x.color;
   this.hungry = (x.hungry!==undefined)?x.hungry : true;
+  // this.hungry = (x.hungry === undefined)? : true;
+
 }
 export default Dog;
